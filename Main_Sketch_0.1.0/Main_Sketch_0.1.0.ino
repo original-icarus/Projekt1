6fbf6dd9de8fa89a62e3423b6b364feb017a88ed
Niklas Waldmann <n.waldmann@tu-braunschweig.de>
2019-05-10 22:11:07 +0200

    Hauptprogramm
    Funktionsdeklaration

--- Main_Sketch_0.1.0/Main_Sketch_0.1.0.ino
+++ Main_Sketch_0.1.0/Main_Sketch_0.1.0.ino
@@ -1,3 +1,13 @@
+// Funktionsdeklaration:
+  // Funktion_1: Temperatur auslesen
+  // Funktion_2: Lichtstärke auslesen
+  // Funktion_3: Stepper
+  // Funktion_4: Servo
+  // Funktion_5: Luftfeuchtigkeit auslesen
+  // Funktion_6: Displayausgabe
+  // Funktion_7: Ladestatus
+  // Funktion_8: Manuelle Steuerung
+  // Funktion_9: Menü
 void setup() {
   // put your setup code here, to run once:
 
